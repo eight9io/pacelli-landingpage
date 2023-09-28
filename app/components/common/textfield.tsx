a6bbbcbcbdf5955ba0069d11d61ca0c4cb3033df
trungdpe9 <trung@lab.e9.digital>
2023-09-28 13:11:54 +0700

    feat: add contact page

--- app/components/common/textfield.tsx
+++ app/components/common/textfield.tsx
@@ -1,11 +1,16 @@
 import clsx from 'clsx';
-import { Field, FieldProps, FieldRenderProps } from 'react-final-form';
+import {Field, FieldProps, FieldRenderProps} from 'react-final-form';
 
 interface TextFieldProp {
   className?: string;
   name: string;
   label?: string;
-  validate?: FieldProps<any, FieldRenderProps<any, HTMLElement, any>, HTMLElement, any>;
+  validate?: FieldProps<
+    any,
+    FieldRenderProps<any, HTMLElement, any>,
+    HTMLElement,
+    any
+  >;
   inputClassName?: string;
   inputErrorClassName?: string;
 }
@@ -19,23 +24,30 @@ const TextField: React.FC<TextFieldProp> = ({
 }) => {
   return (
     <Field name={name}>
-      {({ input, meta }) => (
-        <label htmlFor={`id-${name}`} className={clsx('group relative pb-6', className)}>
+      {({input, meta}) => (
+        <label
+          htmlFor={`id-${name}`}
+          className={clsx('group relative pb-6', className)}
+        >
           <input
             id={`id-${name}`}
             type="text"
             placeholder={label}
             className={clsx(
               'input focus:outline-offset-0 focus:outline-w-[1px] focus:outline-1 rounded-sm input-primary w-full',
-              meta.error && meta.touched && 'focus:outline-red-500 border-red-500',
+              meta.error &&
+                meta.touched &&
+                'focus:outline-red-500 border-red-500',
               meta.error && meta.touched && inputErrorClassName,
-              inputClassName
+              inputClassName,
             )}
             {...input}
           />
           <span className="hidden group-focus:block">{label}</span>
           {meta.error && meta.touched && (
-            <span className="text-red-500 text-sm absolute text-left mt-2 pl-2 left-1 bottom-0">{meta.error}</span>
+            <span className="text-red-500 text-sm absolute text-left mt-2 pl-2 left-1 bottom-0">
+              {meta.error}
+            </span>
           )}
         </label>
       )}
