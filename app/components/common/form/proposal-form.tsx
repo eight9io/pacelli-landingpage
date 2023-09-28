454a0561b4e2dcf9d27001745e2997ab0583c99e
phamthao567 <thao@lab.e9.digital>
2023-09-28 14:34:13 +0700

    fix UI select

--- app/components/common/form/proposal-form.tsx
+++ app/components/common/form/proposal-form.tsx
@@ -2,14 +2,15 @@
 /* eslint-disable */
 import { Button } from '~/components/snippets';
 import DatePicker from '../date-picker';
-import { Form } from 'react-final-form';
+import { Field, Form } from 'react-final-form';
 import TextArea from '~/components/common/text-area';
 import TextField from '~/components/common/textfield';
 import clsx from 'clsx';
 import { contactValidate } from '~/validation/contact';
 import { validateFormValues } from '~/validation';
 import Select from '../select';
-
+import LocaleSwitcher from '~/components/common/languages-selector';
+import { proposalValidate } from '~/validation/proposal';
 interface ProposalFormProps {
   className?: string;
 }
@@ -18,52 +19,66 @@ const ProposalForm: React.FC<ProposalFormProps> = ({ className = '' }) => {
   const onSubmit = (values: any) => {
     console.log('values', values);
   };
-
+  const handleChangeValue = (e: any, value: string, form: any) => {
+    form.change('occupation', value);
+  };
   return (
     <div className={clsx('bg-base-100 px-4 md:px-8 py-14', className)}>
+
       <Form
         onSubmit={onSubmit}
-        validate={validateFormValues(contactValidate)}
+        validate={validateFormValues(proposalValidate)}
         validateOnBlur={false}
-        render={({ handleSubmit }) => (
-          <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
-            <h2 className="text-[32px] md:text-[42px] font-semibold text-[#142423] text-center mb-14">
-              Ricevere una proposta
-            </h2>
-            <Select name="occupation"
-              label="Chi sei?"
+        render={({ handleSubmit, values, form }) => {
+          // console.log("🚀 ~ file: proposal-form.tsx:31 ~ submitErrors:", values)
+          return (
+            <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+              <h2 className="text-[32px] md:text-[42px] font-semibold text-[#142423] text-center mb-14">
+                Ricevere una proposta
 
-              className={clsx(
-                'border-[0px] border-b !border-solid !rounded-none focus:outline-transparent focus:border-b-2',
-              )}
-              inputErrorClassName="focus:border-b-red-500" />
+              </h2>
+              <div className='relative'>
+                <Field name="occupation">
 
-            <TextField
-              name="name"
-              label="Nome  *"
-              inputClassName={clsx(
-                'border-[0px] border-b !border-solid !rounded-none focus:outline-transparent focus:border-b-2 ',
-              )}
-              inputErrorClassName="focus:border-b-red-500"
-            />
+                  {({ input, meta }) => (
+                    <div className='pb-6'>
+                      <Select
+                        value={values?.occupation}
+                        label="Chi sei?"
+                        handleChangeValue={(e: any, value: any) => handleChangeValue(e, value, form)}
+                      />
+                      {meta.touched && meta.error && <span className="absolute text-red-500 text-sm left-1 bottom-0 text-left pl-3 "> {meta.error}</span>}</div>
 
-            <TextField
-              name="email" z
-              label="Email *"
-              inputClassName={clsx(
-                'border-[0px] border-b !border-solid !rounded-none focus:outline-transparent   focus:border-b-2',
-              )}
-              inputErrorClassName="focus:border-b-red-500"
-            />
+                  )}
+                </Field>
+              </div>
+              <TextField
+                name="name"
+                label="Nome  *"
+                inputClassName={clsx(
+                  'border-[0px] border-b !border-solid !rounded-none focus:outline-transparent focus:border-b-2 ',
+                )}
+                inputErrorClassName="focus:border-b-red-500"
+              />
+
+              <TextField
+                name="email"
+                label="Email *"
+                inputClassName={clsx(
+                  'border-[0px] border-b !border-solid !rounded-none focus:outline-transparent   focus:border-b-2',
+                )}
+                inputErrorClassName="focus:border-b-red-500"
+              />
 
 
-            <Button className="rounded-sm uppercase mt-6" size="md">
-              DOWNLOAD
-            </Button>
-          </form>
-        )}
-      ></Form>
-    </div>
+              <Button className="rounded-sm uppercase mt-6" size="md">
+                DOWNLOAD
+              </Button>
+            </form>
+          )
+        }}
+      ></Form >
+    </div >
   );
 };
 
