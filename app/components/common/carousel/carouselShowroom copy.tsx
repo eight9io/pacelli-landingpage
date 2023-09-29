61ff1fd5e5a9c52d69eec0bfdcb1c71888a649d3
phamthao567 <thao@lab.e9.digital>
2023-09-29 16:55:56 +0700

    showroom UI

--- app/components/common/carousel/carouselShowroom copy.tsx
+++ app/components/common/carousel/carouselShowroom copy.tsx
@@ -1,27 +1,18 @@
 'use client';
 /* eslint-disable */
 import clsx from 'clsx';
-import {useCallback, useRef, useState} from 'react';
-import {useDebounce} from 'usehooks-ts';
+import { useCallback, useRef, useState } from 'react';
+import { useDebounce } from 'usehooks-ts';
 
 interface CarouselProps {
   className?: string;
   data?: any[];
-  renderItem?: (
-    item?: any,
-    index?: number,
-    active?: boolean,
-  ) => React.ReactNode;
-  renderIndicator?: (
-    item?: any,
-    index?: number,
-    active?: boolean,
-    onClick?: any,
-  ) => React.ReactNode;
+  renderItem?: (item?: any, index?: number, active?: boolean) => React.ReactNode;
+  renderIndicator?: (item?: any, index?: number, active?: boolean, onClick?: any) => React.ReactNode;
   indicatorClassName?: string;
 }
 
-const Carousel: React.FC<CarouselProps> = ({
+const CarouselArrow: React.FC<CarouselProps> = ({
   className = '',
   data = [],
   renderItem,
@@ -35,9 +26,7 @@ const Carousel: React.FC<CarouselProps> = ({
   const onIndicatorClick = (id: string, index: number = 0) => {
     if (carouselRef.current) {
       console.log(document.getElementById(id)?.offsetLeft);
-      carouselRef.current.scrollTo({
-        left: document.getElementById(id)?.offsetLeft,
-      });
+      carouselRef.current.scrollTo({ left: document.getElementById(id)?.offsetLeft });
       setCurrentIndex(index);
     }
   };
@@ -46,30 +35,20 @@ const Carousel: React.FC<CarouselProps> = ({
     item?: any,
     index?: number,
     active?: boolean,
-    onClick?: (id: string, index?: number) => void,
+    onClick?: (id: string, index?: number) => void
   ) => React.ReactNode = useCallback(
-    (
-      item: any,
-      index?: number,
-      active?: boolean,
-      onClick?: (id: string, index?: number) => void,
-    ) => (
+    (item: any, index?: number, active?: boolean, onClick?: (id: string, index?: number) => void) => (
       <span
         key={`indicator-${item.id}`}
-        onClick={() =>
-          onClick ? onClick(item.id, index) : onIndicatorClick(item.id, index)
-        }
+        onClick={() => (onClick ? onClick(item.id, index) : onIndicatorClick(item.id, index))}
         className="py-1 w-10 cursor-pointer"
       >
         <span
-          className={clsx(
-            'h-0.5 inline-block w-10 cursor-pointer shadow',
-            active ? 'bg-white' : 'bg-gray-400',
-          )}
+          className={clsx('h-0.5 inline-block w-10 cursor-pointer shadow', active ? 'bg-white' : 'bg-gray-400')}
         ></span>
       </span>
     ),
-    [],
+    []
   );
 
   return (
@@ -91,33 +70,18 @@ const Carousel: React.FC<CarouselProps> = ({
             renderItem(item, index)
           ) : (
             <div id={item.id} key={item.id} className="carousel-item w-full">
-              <img
-                src={item.src}
-                className="w-full object-cover object-center"
-              />
+              <img src={item.src} className="w-full object-cover object-center" />
             </div>
-          ),
+          )
         )}
       </div>
-      <div
-        className={clsx(
-          'absolute z-100 bottom-24 left-0 w-full',
-          indicatorClassName,
-        )}
-      >
+      <div className={clsx('absolute z-100 bottom-24 left-0 w-full', indicatorClassName)}>
         <div className={clsx('base-container flex justify-start py-2 gap-2')}>
-          {data.map((item, index) =>
-            (renderIndicator ?? defaultRenderIndicator)(
-              item,
-              index,
-              index === debounceIndex,
-              onIndicatorClick,
-            ),
-          )}
+          {renderIndicator && renderIndicator()}
         </div>
       </div>
     </>
   );
 };
 
-export default Carousel;
+export default CarouselArrow;
---
+++ app/routes/($locale).showroom.tsx
@@ -0,0 +1,14 @@
+
+import Showroom from '~/components/showroom/Showroom';
+import CarouselShowroom from '~/components/showroom/CarouselArrow';
+import MyTeam from '~/components/showroom/MyTeam';
+export default function PartnershipServicesPage() {
+  return (
+    <>
+
+      <Showroom />
+      <CarouselShowroom />
+      <MyTeam />
+    </>
+  );
+}
