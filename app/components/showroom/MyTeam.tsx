eb1fbac1d43ca13ee63bef569f58d4631adda338
phamthao567 <thao@lab.e9.digital>
2023-10-02 14:59:05 +0700

    fix modal

--- app/components/showroom/MyTeam.tsx
+++ app/components/showroom/MyTeam.tsx
@@ -16,12 +16,20 @@ const MyTeam: React.FC<MyTeamProps> = ({ className = '' }) => {
   const [openForm, setOpenForm] = useState(false)
   const openPopup = () => {
     setOpenForm(true);
-    document.body.style.overflow = 'hidden';
+    document.body.style.overflow = 'hidden'
+    document.body.classList.add("lg:pr-[15px]")
+    var header = document.getElementById('nav-header')
+    if (header) header.classList.add("lg:pr-[47px]")
+
   };
 
   const closePopup = () => {
     setOpenForm(false);
     document.body.style.overflow = 'auto';
+    document.body.classList.remove("lg:pr-[15px]")
+    var header = document.getElementById('nav-header')
+    if (header) header.classList.remove("lg:pr-[47px]")
+
   };
   return (
     <section className={clsx('bg-white  py-[60px] md:py-[90px]', className)}>
@@ -43,14 +51,15 @@ const MyTeam: React.FC<MyTeamProps> = ({ className = '' }) => {
             </p>
             <InfoContact />
           </div>
-          <div className="col-span-12 md:col-span-4  flex flex-col 
+          <div className="col-span-12 md:col-span-4  flex flex-col  
      ">
+
             <img
               src={showroom6}
               alt="Mission"
               width={394}
               height={500}
-              className="h-[500px]" />
+              className="h-[500px] w-full " />
 
             <h3 className="text-gray-900 text-[40px] font-normal leading-[50px] mt-6 mb-4">
               Bruno
@@ -70,14 +79,15 @@ const MyTeam: React.FC<MyTeamProps> = ({ className = '' }) => {
 
             </div>
           </div>
-          <div className="col-span-12 md:col-span-4  flex flex-col 
+          <div className="col-span-12 md:col-span-4  flex flex-col justify-center items-center  
          ">
+
             <img
               src={showroom6}
               alt="Mission"
               width={394}
               height={500}
-              className="h-[500px]" />
+              className="h-[500px] w-full" />
 
             <h3 className="text-gray-900 text-[40px] font-normal leading-[50px] mt-6 mb-4">
               Ernesto
@@ -88,19 +98,20 @@ const MyTeam: React.FC<MyTeamProps> = ({ className = '' }) => {
             </p>
             <div className='w-full mt-auto'>
               <Button
-                className="rounded-sm uppercase flex gap-3 mt-12    "
+                className="rounded-sm uppercase flex gap-3 mt-12  "
                 size="md"
                 onClick={openPopup}>
                 Book appointment
                 <ArrowRight className="text-secondary w-5 h-5 stroke-secondary origin-center stroke-2" />
               </Button>
-
             </div>
           </div>
+
         </div>
       </div>
       {
-        openForm && <div className='fixed  w-screen h-screen bg-[#57575799] top-0 left-0 z-20 flex justify-center items-center  '
+        openForm && <div className={clsx('fixed  w-screen h-screen bg-[#57575799] top-0 left-0 z-20 flex justify-center items-center overflow-x-hidden  ',
+          openForm ? 'show-popup active' : 'hide-popup')}
           onClick={closePopup}>
           <div
             onClick={(e) => e.stopPropagation()}
