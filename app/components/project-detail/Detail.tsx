c7183f4f765eb49a3b52255924bda203fe4f5a56
phamthao567 <thao@lab.e9.digital>
2023-10-04 15:05:25 +0700

    done UI

---
+++ app/components/project-detail/Detail.tsx
@@ -0,0 +1,81 @@
+/* eslint-disable */
+import clsx from 'clsx';
+import { useTranslation } from 'react-i18next';
+
+interface DetailProps {
+    className?: string;
+    title: string;
+    subTitle: string;
+    imgPosition?: 'left' | 'right';
+    imageIntro: Array<string>;
+}
+
+const Detail: React.FC<DetailProps> = ({
+    className = '',
+    title,
+    subTitle,
+    imageIntro,
+    imgPosition = "right",
+}) => {
+    const { t } = useTranslation('home');
+
+    return (
+        <section
+            className={clsx(' pt-[30px] lg:pt-[60px] base-container ', className)}
+        >
+            <h2 className="text-[40px] md:text-[64px] font-semibold leading-[50px] md:leading-[78px] text-primary-950 ">
+                {title}
+            </h2>
+            <div
+                className={clsx('grid grid-cols-12  gap-y-8 my-8 md:gap-6 lg:gap-11')}
+            >
+                <div
+                    className={clsx(
+                        'col-span-12  lg:col-span-4',
+                        imgPosition === 'right'
+                            ? 'md:col-span-5 lg:col-span-4'
+                            : 'md:col-span-7 lg:col-span-8 ',
+                    )}
+                >
+                    {imgPosition === 'right' ? (
+                        <p className="  text-base font-normal leading-7 text-neutral-800 mb-8">
+                            {subTitle}
+                        </p>
+                    ) : (
+                        <img
+                            src={imageIntro[0]}
+                            alt="Mission"
+                            width={715}
+                            height={600}
+                            className="h-[600px] w-full "
+                        />
+                    )}
+                </div>
+                <div
+                    className={clsx(
+                        'col-span-12  ',
+                        imgPosition === 'right'
+                            ? ' md:col-span-7 lg:col-span-8 '
+                            : 'md:col-span-5 lg:col-span-4',
+                    )}
+                >
+                    {imgPosition === 'right' ? (
+                        <img
+                            src={imageIntro[0]}
+                            alt="Mission"
+                            width={715}
+                            height={600}
+                            className="h-[600px] w-full "
+                        />
+                    ) : (
+                        <p className="  text-base font-normal leading-7 text-neutral-800 mb-8">
+                            {subTitle}
+                        </p>
+                    )}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Detail;
