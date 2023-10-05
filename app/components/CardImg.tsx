716ec4fe29a9a782b4706ab133e3a6f82fdddaa8
tientien1103 <le.nttien99@gmail.com>
2023-10-05 18:05:19 +0700

    add pagination in gallery page

--- app/components/CardImg.tsx
+++ app/components/CardImg.tsx
@@ -14,15 +14,15 @@ const CardImg = ({item}: ICard) => {
   return (
     <div className=" overflow-hidden shadow-lg">
       <img
-        className="w-full"
+        className="w-full h-[225px] md:h-[400px] object-cover"
         src={item.cover_images[0] || ''}
         alt={item.title}
       />
       <div className="px-8 py-5">
-        <h3 className="text-primary text-[40px] font-semibold leading-[50px]">
+        <h3 className="text-primary text-[40px] line-clamp-1 font-semibold leading-[50px]">
           {item.title}
         </h3>
-        <p className=" text-gray-900 text-base font-normal leading-7 mt-3">
+        <p className=" text-gray-900 text-base font-normal line-clamp-3 leading-7 mt-3">
           {item.description}
         </p>
         <Button
