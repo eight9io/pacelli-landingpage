952431f9650f85a982e37a8a54e636e0256d8113
tientien1103 <le.nttien99@gmail.com>
2023-10-05 18:05:19 +0700

    add pagination in gallery page

--- app/components/gallery/OutProjects.tsx
+++ app/components/gallery/OutProjects.tsx
@@ -4,11 +4,14 @@ import project1 from '~/assets/gallery/project-1.png';
 import project2 from '~/assets/gallery/project-2.png';
 import project3 from '~/assets/gallery/project-3.png';
 import CardImg from '../CardImg';
+import {useState} from 'react';
 interface ContactProps {
   className?: string;
   projects: any[];
   pageInfo: any;
 }
+
+const PROJECTS_PER_PAGE = 6;
 /* eslint-disable */
 const OutProjects: React.FC<ContactProps> = ({
   className = '',
@@ -16,7 +19,23 @@ const OutProjects: React.FC<ContactProps> = ({
   pageInfo,
 }) => {
   const {t} = useTranslation('home');
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
+  const totalProjects = projects.length;
+  const lastProjectIndex = currentPage * PROJECTS_PER_PAGE;
+  const firstProjectIndex = 0;
+
+  let pages = [];
+  const nextPage = () => {
+    if (currentPage === pages.length) return;
+
+    setCurrentPage(currentPage + 1);
+  };
+
+  if (totalProjects)
+    for (let i = 1; i <= Math.ceil(totalProjects / PROJECTS_PER_PAGE); i++) {
+      pages.push(i);
+    }
   return (
     <section
       className={clsx(
@@ -30,16 +49,30 @@ const OutProjects: React.FC<ContactProps> = ({
         completed projects
       </h2>
       <div className="grid grid-cols-12 gap-y-8 lg:gap-8 mt-11 ">
-        {projects.map((item, index) => (
-          <div className="col-span-12 lg:col-span-6" key={item.id}>
-            <CardImg item={item} />
-          </div>
-        ))}
+        {projects
+          .slice(firstProjectIndex, lastProjectIndex)
+          .map((item, index) => (
+            <div className="col-span-12 lg:col-span-6" key={item.id}>
+              <CardImg item={item} />
+            </div>
+          ))}
       </div>
       {pageInfo.hasNextPage === true && (
-        <p className=" cursor-pointer text-center text-secondary font-normal my-8  mx-auto text-4xl leading-10  underline">
-          Load more
-        </p>
+        <div className="text-center">
+          <button
+            disabled={currentPage === pages.length}
+            onClick={nextPage}
+            className={clsx(
+              'text-secondary cursor-pointer font-normal my-8 mx-auto text-4xl leading-10 underline',
+              {
+                hidden: currentPage === pages.length,
+                block: currentPage !== pages.length,
+              },
+            )}
+          >
+            Load more
+          </button>
+        </div>
       )}
     </section>
   );
