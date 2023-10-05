ce4a98d7dfd80cc61eb404131e15c30fbb37320b
tientien1103 <le.nttien99@gmail.com>
2023-10-05 16:26:35 +0700

    update projects in gallery

--- app/components/gallery/OutProjects.tsx
+++ app/components/gallery/OutProjects.tsx
@@ -1,57 +1,28 @@
 import clsx from 'clsx';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 import project1 from '~/assets/gallery/project-1.png';
 import project2 from '~/assets/gallery/project-2.png';
 import project3 from '~/assets/gallery/project-3.png';
 import CardImg from '../CardImg';
 interface ContactProps {
   className?: string;
+  projects: any[];
+  pageInfo: any;
 }
 /* eslint-disable */
-const mockData = [
-  {
-    id: 'project-1',
-    image: project1,
-    name: 'Lakefront Retreat',
-    subtitle: 'Nunc cum aliquet sagittis duis ipsum tincidunt sed scelerisque. Viverra tortor, sit risus bibendum faucibus vitae metus, in. Sit id dictum aliquam at cras eu.'
-  },
-  {
-    id: 'project-2',
-    image: project2,
-    name: 'Lakefront Retreat',
-    subtitle: 'Nunc cum aliquet sagittis duis ipsum tincidunt sed scelerisque. Viverra tortor, sit risus bibendum faucibus vitae metus, in. Sit id dictum aliquam at cras eu.'
-  },
-  {
-    id: 'project-3',
-    image: project3,
-    name: 'Lakefront Retreat',
-    subtitle: 'Nunc cum aliquet sagittis duis ipsum tincidunt sed scelerisque. Viverra tortor, sit risus bibendum faucibus vitae metus, in. Sit id dictum aliquam at cras eu.'
-  },
-  {
-    id: 'project-4',
-    image: project3,
-    name: 'Lakefront Retreat',
-    subtitle: 'Nunc cum aliquet sagittis duis ipsum tincidunt sed scelerisque. Viverra tortor, sit risus bibendum faucibus vitae metus, in. Sit id dictum aliquam at cras eu.'
-  },
-  {
-    id: 'project-5',
-    image: project3,
-    name: 'Lakefront Retreat',
-    subtitle: 'Nunc cum aliquet sagittis duis ipsum tincidunt sed scelerisque. Viverra tortor, sit risus bibendum faucibus vitae metus, in. Sit id dictum aliquam at cras eu.'
-  },
-  {
-    id: 'project-6',
-    image: project3,
-    name: 'Lakefront Retreat',
-    subtitle: 'Nunc cum aliquet sagittis duis ipsum tincidunt sed scelerisque. Viverra tortor, sit risus bibendum faucibus vitae metus, in. Sit id dictum aliquam at cras eu.'
-  },
-]
-const OutProjects: React.FC<ContactProps> = ({ className = '' }) => {
-  const { t } = useTranslation('home');
+const OutProjects: React.FC<ContactProps> = ({
+  className = '',
+  projects,
+  pageInfo,
+}) => {
+  const {t} = useTranslation('home');
 
   return (
     <section
-      className={clsx(' py-[30px] lg:pt-[90px] base-container mt-12', className)}
+      className={clsx(
+        ' py-[30px] lg:pt-[90px] base-container mt-12',
+        className,
+      )}
     >
       <h2 className="text-[40px] md:text-[64px] font-semibold leading-[50px] md:leading-[78px] text-[#142423] lg:w-1/2">
         Our projects <br />
@@ -59,13 +30,17 @@ const OutProjects: React.FC<ContactProps> = ({ className = '' }) => {
         completed projects
       </h2>
       <div className="grid grid-cols-12 gap-y-8 lg:gap-8 mt-11 ">
-        {mockData.map((item, index) => <div className="col-span-12 lg:col-span-6   " key={item.id}>
-          <CardImg item={item} />
-        </div>)}
+        {projects.map((item, index) => (
+          <div className="col-span-12 lg:col-span-6" key={item.id}>
+            <CardImg item={item} />
+          </div>
+        ))}
       </div>
-      <p className=" cursor-pointer text-center text-secondary font-normal my-8  mx-auto text-4xl leading-10  underline">
-        Load more
-      </p>
+      {pageInfo.hasNextPage === true && (
+        <p className=" cursor-pointer text-center text-secondary font-normal my-8  mx-auto text-4xl leading-10  underline">
+          Load more
+        </p>
+      )}
     </section>
   );
 };
