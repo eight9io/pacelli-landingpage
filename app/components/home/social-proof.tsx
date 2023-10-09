d3f26780b953195d7b4475362c393599ea70e7bd
phamthao567 <thao@lab.e9.digital>
2023-10-09 10:23:11 +0700

    add img service

--- app/components/home/social-proof.tsx
+++ app/components/home/social-proof.tsx
@@ -2,12 +2,17 @@ import Carousel from '~/components/common/carousel';
 import IconQuote from '~/components/common/icons/quote';
 import clsx from 'clsx';
 import Heading from '../common/heading';
-
+import maria from '~/assets/carousel/maria.png';
+import maraja from '~/assets/carousel/maraja.png';
+import franco from '~/assets/carousel/franco.png';
+import lino from '~/assets/carousel/lino.png';
+import agostino from '~/assets/carousel/agostino.png';
+import geltrude from '~/assets/carousel/geltrude.png';
 interface SocialProofProps {
   className?: string;
 }
 
-const SocialProof: React.FC<SocialProofProps> = ({className = ''}) => {
+const SocialProof: React.FC<SocialProofProps> = ({ className = '' }) => {
   return (
     <section className={clsx('pt-[60px] bg-base-100', className)}>
       <Heading className="text-center">Cosa dicono di noi</Heading>
@@ -22,7 +27,7 @@ const SocialProof: React.FC<SocialProofProps> = ({className = ''}) => {
             >
               <div className="col-span-12 md:col-span-8 px-4 md:px-0">
                 <img
-                  className="w-full h-[400px]"
+                  className="h-[400px]"
                   src={item.image}
                   alt={item.author}
                   width={754}
@@ -60,48 +65,42 @@ interface SocialProofItemProps {
 const mockData: SocialProofItemProps[] = [
   {
     id: 'item-proof1',
-    image:
-      'https://cdn.shopify.com/s/files/1/0816/1971/4346/files/home_hero_1.png',
+    image: geltrude,
     author: 'Geltrude',
     quote:
       'Felice di essermi affidata a voi che siete stati super professionali e siete riusciti ad incontrare il mio gusto creando con molto garbo il laboratorio che ho sempre sognato.',
   },
   {
     id: 'item-proof2',
-    image:
-      'https://cdn.shopify.com/s/files/1/0816/1971/4346/files/home_hero_1.png',
+    image: maria,
     author: 'Maria',
     quote:
       'Paolo un grande professionista. Sa consigliarti sempre con garbo e consapevolezza del suo sapere!',
   },
   {
     id: 'item-proof3',
-    image:
-      'https://cdn.shopify.com/s/files/1/0816/1971/4346/files/home_hero_1.png',
+    image: maraja,
     author: 'Marajà',
     quote:
       'Grazie Paolo che hai realizzato perfettamente la nostra idea. Complimenti per la professionalità, la gentilezza e la precisione. Grazie angora da tutta la famiglia Marajà.',
   },
   {
     id: 'item-proof4',
-    image:
-      'https://cdn.shopify.com/s/files/1/0816/1971/4346/files/home_hero_1.png',
+    image: franco,
     author: 'Franco',
     quote:
       'Grazie ad  Arredamenti Pacelli per la professionalità e competenza nella progettazione e montaggio della mia nuova cucina , ho ricevuto tantissimi complimenti dai miei amici.',
   },
   {
     id: 'item-proof5',
-    image:
-      'https://cdn.shopify.com/s/files/1/0816/1971/4346/files/home_hero_1.png',
+    image: lino,
     author: 'Lino',
     quote:
       'La professionalità fatta persona, persona bellissima. Ho avuto il piacere di collaborare con loro, passione sensa fine.',
   },
   {
     id: 'item-proof6',
-    image:
-      'https://cdn.shopify.com/s/files/1/0816/1971/4346/files/home_hero_1.png',
+    image: agostino,
     author: 'Agostino',
     quote: 'Grazie per l’immensa professionalità.',
   },
