d2b448a36edb19e7de7ef492fbfd627a0ee2b1d3
trungdpe9 <trung@lab.e9.digital>
2023-10-10 14:50:16 +0700

    feat: fix gallery detail

--- app/components/CardImg.tsx
+++ app/components/CardImg.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import {Button} from './Button';
 import ArrowRight from '~/components/common/icons/arrow-right';
-/* eslint-disable */
+import Link from './Link';
+
 interface ICard {
   item: {
     id: string;
     title: string;
     description: string;
-    cover_images: string[];
+    cover: string[];
+    handle: string;
   };
 }
 const CardImg = ({item}: ICard) => {
   return (
-    <div className=" overflow-hidden shadow-lg">
+    <div className=" overflow-hidden shadow-lg h-full">
       <img
         className="w-full h-[225px] md:h-[400px] object-cover"
-        src={item.cover_images[0] || ''}
+        src={item.cover && item.cover.length ? item.cover[0] : ''}
         alt={item.title}
       />
+
       <div className="px-8 py-5">
         <h3 className="text-primary text-[40px] line-clamp-1 font-semibold leading-[50px]">
           {item.title}
@@ -25,13 +26,13 @@ const CardImg = ({item}: ICard) => {
         <p className=" text-gray-900 text-base font-normal line-clamp-3 leading-7 mt-3">
           {item.description}
         </p>
-        <Button
-          className="uppercase mt-8 flex gap-3  bg-secondary-900 font-bold text-center py-2.5 px-4 text-white my-4 hover:bg-secondary-800  "
-          size="md"
+        <Link
+          className="uppercase mt-8 inline-flex gap-3 bg-secondary-900 font-bold text-center py-2.5 px-4 text-white my-4 hover:bg-secondary-800  "
+          to={`/project/${item.handle}`}
         >
-          VIEW more
+          view more
           <ArrowRight className="text-secondary w-5 h-5 stroke-secondary origin-center stroke-2" />
-        </Button>
+        </Link>
       </div>
     </div>
   );
