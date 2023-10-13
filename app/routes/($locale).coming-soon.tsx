d44b25c41e0007ffad0d2ecebbdc31147caa7479
trungdpe9 <trung@lab.e9.digital>
2023-10-13 21:48:22 +0700

    feat: coming-soon

--- app/routes/($locale).coming-soon.tsx
+++ app/routes/($locale).coming-soon.tsx
@@ -1,14 +1,21 @@
 import Heading from '~/components/common/heading';
-import comming_soon from '~/assets/images/image_coming_soon.png';
+import logo from '~/assets/images/logo-4x.png';
 
 export default function ComingSoonPage() {
   return (
     <>
       <section className="p-0 bg-secondary">
         <div className="base-container flex flex-col justify-center items-center min-h-screen">
-          <img src={comming_soon} alt="pacelli" />
-          <Heading className="mb-2 text-white">Prossimamente!!!</Heading>
-          <p className="text-center text-gray-400 text-base font-normal leading-7">
+          <img
+            src={logo}
+            className="mb-8 md:w-[500px]"
+            alt="Coming soon"
+            width={608}
+          />
+          <Heading className="text-primary mb-2 !text-[32px] md:!text-[40px] font-semibold">
+            Prossimamente!!!
+          </Heading>
+          <p className="text-center text-gray-600 text-base font-normal leading-7">
             Presto lanceremo il nostro sito web.
           </p>
         </div>
