60fbe7bb665724cfa191b2c560be0bea7cf4ec99
trungdpe9 <trung@lab.e9.digital>
2023-10-13 21:37:08 +0700

    feat: coming soon

--- app/routes/($locale).coming-soon.tsx
+++ app/routes/($locale).coming-soon.tsx
@@ -1,15 +1,15 @@
 import Heading from '~/components/common/heading';
 import comming_soon from '~/assets/images/image_coming_soon.png';
 
-export default function PartnershipServicesPage() {
+export default function ComingSoonPage() {
   return (
     <>
       <section className="p-0 bg-secondary">
         <div className="base-container flex flex-col justify-center items-center min-h-screen">
-          <img src={comming_soon} className="mb-8" alt="Coming soon" />
-          <Heading className="text-white mb-2">Coming soon!!!</Heading>
+          <img src={comming_soon} alt="pacelli" />
+          <Heading className="mb-2 text-white">Prossimamente!!!</Heading>
           <p className="text-center text-gray-400 text-base font-normal leading-7">
-            We will soon be launching our website.
+            Presto lanceremo il nostro sito web.
           </p>
         </div>
       </section>
