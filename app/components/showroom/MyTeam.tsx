6e8da37f2b23a5fd5918a59fbf6af1ffec5cedbc
phamthao567 <thao@lab.e9.digital>
2023-10-17 17:51:57 +0700

    fix-modal

--- app/components/showroom/MyTeam.tsx
+++ app/components/showroom/MyTeam.tsx
@@ -121,7 +121,7 @@ const MyTeam: React.FC<MyTeamProps> = ({ className = '' }) => {
             className="md:scale-[0.85] max-w-[608px] base-container z-50 absolute"
           >
             <BookingForm handleClose={closePopup} closeButton={
-              <XMarkIcon stroke="gray" className="h-6 w-6  absolute top-5 right-5 cursor-pointer hover:scale-125 transition-all duration-300" aria-hidden="true"
+              <XMarkIcon stroke="gray" className="h-8 w-8  absolute top-5 right-5 cursor-pointer hover:scale-125 transition-all duration-300 z-[100]" aria-hidden="true"
                 onClick={closePopup}
               />
             } />
