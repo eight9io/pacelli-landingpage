388927855bf04a7a9e1ff8621d573b8287398b6a
phamthao567 <thao@lab.e9.digital>
2023-10-17 09:25:27 +0700

    i18n

--- app/components/common/form/proposal-form.tsx
+++ app/components/common/form/proposal-form.tsx
@@ -1,15 +1,16 @@
-import {Button} from '~/components/snippets';
-import {Field, Form} from 'react-final-form';
+import { Button } from '~/components/snippets';
+import { Field, Form } from 'react-final-form';
 import TextField from '~/components/common/textfield';
 import clsx from 'clsx';
-import {validateFormValues} from '~/validation';
+import { validateFormValues } from '~/validation';
 import Select from '../select';
-import {proposalValidate} from '~/validation/proposal';
+import { proposalValidate } from '~/validation/proposal';
 import Heading from '../heading';
 import ReCAPTCHA from 'react-google-recaptcha';
-import {useRef, useState} from 'react';
-import {useRootContext} from '~/hooks/useRootContext';
-import {FormApi} from 'final-form';
+import { useRef, useState } from 'react';
+import { useRootContext } from '~/hooks/useRootContext';
+import { FormApi } from 'final-form';
+import { useTranslation } from 'react-i18next';
 
 interface ProposalFormProps {
   className?: string;
@@ -25,17 +26,18 @@ const pdfLink =
   'https://cdn.shopify.com/s/files/1/0816/1971/4346/files/pacelii.pdf';
 
 /* eslint-disable */
-const ProposalForm: React.FC<ProposalFormProps> = ({className = ''}) => {
+const ProposalForm: React.FC<ProposalFormProps> = ({ className = '' }) => {
+  const { t } = useTranslation('common');
   const [submitted, setSubmitted] = useState(false);
   const [loading, setLoading] = useState(false);
   const [reCaptchaDone, setReCaptchaDone] = useState(false);
   const recaptchaRef = useRef<ReCAPTCHA>(null);
 
-  const onChange = () => {
+  const onChange = (value: any) => {
     setReCaptchaDone(true);
   };
 
-  const {ENV} = useRootContext();
+  const { ENV } = useRootContext();
 
   const onSubmit = (
     values: any,
@@ -95,20 +97,20 @@ const ProposalForm: React.FC<ProposalFormProps> = ({className = ''}) => {
     <div className={clsx('bg-base-100 px-4 md:px-8 py-14 relative', className)}>
       <Form
         onSubmit={onSubmit}
-        validate={validateFormValues(proposalValidate)}
+        validate={validateFormValues(proposalValidate(t))}
         validateOnBlur={false}
-        render={({handleSubmit, values, form}) => {
+        render={({ handleSubmit, values, form }) => {
           return (
             <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
               <Heading
                 className="font-semibold text-primary text-center mb-14"
                 variant="h3"
               >
-                Ricevere una proposta
+                {t('proposal_form.title')}
               </Heading>
               <div className="relative">
                 <Field name="occupation">
-                  {({input, meta}) => (
+                  {({ input, meta }) => (
                     <div className="pb-6">
                       <Select
                         value={values?.occupation}
@@ -164,7 +166,7 @@ const ProposalForm: React.FC<ProposalFormProps> = ({className = ''}) => {
                 size="md"
                 disabled={loading || submitted}
               >
-                DOWNLOAD
+                {t('button.download')}
               </Button>
               {submitted && (
                 <span className="mb-2 absolute top-6 flex justify-center items-start gap-1 md:gap-2 font-semibold text-sm">
--- app/routes/($locale).services.professional.tsx
+++ app/routes/($locale).services.professional.tsx
@@ -1,18 +1,19 @@
-import {defer, type LoaderArgs} from '@shopify/remix-oxygen';
-import {AnalyticsPageType} from '@shopify/hydrogen';
+import { defer, type LoaderArgs } from '@shopify/remix-oxygen';
+import { AnalyticsPageType } from '@shopify/hydrogen';
 import services1 from '~/assets/services/services-6.png';
 import services2 from '~/assets/services/services-7.png';
-import {seoPayload} from '~/lib/seo.server';
-import {routeHeaders} from '~/data/cache';
+import { seoPayload } from '~/lib/seo.server';
+import { routeHeaders } from '~/data/cache';
 import HeroPartnerships from '~/components/services/partnerships/HeroPartnerships';
 import Contact from '~/components/services/partnerships/Contact';
 import Brands from '~/components/home/brands';
 import SocialProof from '~/components/home/social-proof';
 import BeforeAfter from '~/components/BeforeAfter';
+import { useTranslation } from 'react-i18next';
 
 export const headers = routeHeaders;
 
-export async function loader({params, context}: LoaderArgs) {
+export async function loader({ params, context }: LoaderArgs) {
   const seo = seoPayload.home();
 
   return defer({
@@ -24,13 +25,14 @@ export async function loader({params, context}: LoaderArgs) {
 }
 
 export default function PartnershipServicesPage() {
+  const { t } = useTranslation('professional');
   return (
     <>
       <HeroPartnerships />
       <Contact />
       <BeforeAfter
-        title="Cosa facciamo guarda il prima e il dopo"
-        description="Siamo entusiasti di mostrarti alcuni dei progetti che abbiamo realizzato, tutti personalizzati per soddisfare le esigenze dei nostri clienti. La trasformazione è incredibile, e siamo pronti a portare anche il tuo spazio al livello successivo. Contattaci ora per vedere come possiamo migliorare il tuo ambiente!"
+        title={t('before_after.title')}
+        description={t('before_after.desc')}
         afterImg={services2}
         beforeImg={services1}
       />
