8e20c7325d9d11a6271ee0b9473f7d84c2e23bd7
tientien1103 <le.nttien99@gmail.com>
2023-10-17 09:56:07 +0700

    brand

--- app/components/brand/hero.tsx
+++ app/components/brand/hero.tsx
@@ -1,3 +1,4 @@
+import {useTranslation} from 'react-i18next';
 import Heading from '../common/heading';
 import {Text} from '../Text';
 import clsx from 'clsx';
@@ -8,6 +9,7 @@ interface HeroProps {
 }
 
 const Hero: React.FC<HeroProps> = ({className = ''}) => {
+  const {t} = useTranslation('brand');
   return (
     <section
       className={clsx(
@@ -18,26 +20,10 @@ const Hero: React.FC<HeroProps> = ({className = ''}) => {
       <div className="grid grid-cols-12 base-container gap-8">
         <div className="col-span-12 md:col-span-7">
           <Heading className="mb-4 md:mb-8 text-secondary" variant="h3">
-            I nostri partner sono solo i <br className="hidden md:block" />
-            migliori marchi di arredamento
+            {t('brand:hero.title')}
           </Heading>
           <Text className="mb-6 md:mb-10 text-gray-900">
-            Siamo orgogliosi di affermare che le Aziende di cui siamo
-            rifornitori sono considerate le migliori nel mercato del mobile.
-            Aziende orientate alla ricerca di soluzioni moderne e creative e
-            attente ai cambiamenti del vivere la casa e l&apos;arredo. Il
-            mercato del mobile è noto per la sua continua innovazione ed
-            evoluzione. Per rimanere al passo con le ultime tendenze e
-            tecnologie, è essenziale lavorare con aziende all&apos;avanguardia.
-            Siamo felici di essere rifornitori di aziende leader nel settore del
-            mobile, caratterizzate dalla costante ricerca di soluzioni
-            innovative e creative per l&apos;arredamento domestico e non. La
-            qualità, l&apos;innovazione, l&apos;affidabilità e la collaborazione
-            sono i pilastri della nostra relazione con questi eccezionali
-            partner. In un settore in cui la qualità è fondamentale per
-            migliorare la vita quotidiana dei consumatori, le aziende che
-            serviamo non fanno mai compromessi su questo aspetto, continuando ad
-            offrire standard qualitativi altissimi.
+            {t('brand:hero.description')}
           </Text>
         </div>
         <div className="col-span-12 md:col-span-5">
